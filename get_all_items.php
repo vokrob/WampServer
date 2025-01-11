38a8b3a47827c59631e633dca6252c29ea5dc28c
vokrob <106639488+vokrob@users.noreply.github.com>
2025-01-11 20:51:11 +0700

    Creating a database, retrieving all items from the database

---
+++ get_all_items.php
@@ -0,0 +1,28 @@
+<?php
+
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_name = "test_db";
+$table_name = "users";
+
+$connect = new mysqli($server_name, $user_name, $password, $db_name);
+
+if ($connect -> connect_error) {
+    die("Connection failed: " . $connect -> connect_error);
+}
+
+$sql = "SELECT * FROM $table_name";
+$result = $connect -> query($sql);
+
+if ($result -> num_rows > 0) {
+    $data = array();
+    while($row = $result -> fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo json_encode(array("message" => "List is empty"));
+}
+
+?>
